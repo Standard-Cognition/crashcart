2068c2ed27a887fa760c70df84b33dae7cab327e
Brandon Ogle <brandon@standardcognition.com>
2020-07-31 16:48:28 -0700

    Pin nixpkgs and mozilla sources in the root of the repository

---
+++ nixpkgs.nix
@@ -0,0 +1,36 @@
+{ sources ? import ./nix/sources.nix }:
+
+let
+  rustChannelsOverlay = import "${sources.nixpkgs-mozilla}/rust-overlay.nix";
+  rustChannelsSrcOverlay = import "${sources.nixpkgs-mozilla}/rust-src-overlay.nix";
+
+  rustOverlay = self: super:
+    let channel =
+      super.rustChannelOf {
+        rustToolChain = "nightly-2020-04-21";
+        sha256 = "1ffhardy4xqs3h49wwlbry2hgl0qjfd1kza9bsrk92xvb25yaw2x";
+      };
+      rustNightly = channel.rust.override {
+        extensions = [
+          "clippy-preview"
+          "rls-preview"
+          "rust-analysis"
+          "rustfmt-preview"
+          "rust-src"
+          "rust-std"
+        ];
+      };
+    in {
+      rust = rustNightly;
+      cargo = channel.rust;
+      rustc = channel.rust;
+    };
+in
+import sources.nixpkgs {
+  overlays = [
+      rustChannelsOverlay
+      rustChannelsSrcOverlay
+      rustOverlay
+  ];
+}
+
